8f84c01638d68235ad08f1250d5b01c8d344bbbc
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-06-01 19:06:43 -0300

    player moviments

--- src/GameObjects/player.ts
+++ src/GameObjects/player.ts
@@ -1,31 +1,22 @@
-type playerConfigTypes = {
+export class Player {
+  ctx: CanvasRenderingContext2D
+  color: string
   x: number;
   y: number;
+  h: number; 
   w: number;
-  h: number;
-  draw(): void;
-  update(): void;
-}
-
-function PlayerInit(ctx: CanvasRenderingContext2D) {
-  let player: playerConfigTypes = {
-    x: 0,
-    y: 0,
-    w: 25,
-    h: 25,
-    draw() {
-      const { x, y, w, h } = this
-      ctx.rect(x, y, w, h)
-      ctx.fill()
-      ctx.fillStyle = `rgb(149,117,205)`
-    },
-    update() {
-      ctx.moveTo(this.x += 25, this.y)
-      ctx.closePath()
-    }
+  constructor(ctx: CanvasRenderingContext2D, color: string, x: number, y: number, w: number, h: number) {
+    this.ctx = ctx
+    this.color = color
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
   }
-  return player
-}
-export {
-  PlayerInit
+  draw() {
+    const { x, y, w, h, ctx, color } = this
+    ctx.rect(x, y, w, h)
+    ctx.fill()
+    ctx.fillStyle = color
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,57 @@
-import { PlayerInit } from './GameObjects/player'
-import './Styles/style.css'
+import { Player } from "./GameObjects/player";
+import "./Styles/style.css";
 
 (() => {
-  var canvas = <HTMLCanvasElement>document.querySelector('#canvas')
-  const ctx = <CanvasRenderingContext2D>canvas.getContext('2d')
-  let player = PlayerInit(ctx)
+  var canvas = <HTMLCanvasElement>document.querySelector("#canvas");
+  const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+  let player = new Player(ctx, "rgb(255,202,40)", 0, 0, 25, 25);
 
   function game() {
-    player.draw()
+    player.draw();
 
-    requestAnimationFrame(game)
+    requestAnimationFrame(game);
   }
 
-  window.addEventListener('click', () => {
-    player.update()
-    console.log(player.w, player.x)
-  })
+  const up = <HTMLButtonElement>document.querySelector("#control-up");
+  const down = <HTMLButtonElement>document.querySelector("#control-down");
+  const left = <HTMLButtonElement>document.querySelector("#control-left");
+  const right = <HTMLButtonElement>document.querySelector("#control-right");
 
-  game()
-})()
+  up.onclick = () => {
+    player.y -= 25;
+    player.color = "rgb(179,157,219)";
+  };
+  down.onclick = () => {
+    player.y += 25;
+    player.color = "rgb(239,83,80)";
+  };
+  left.onclick = () => {
+    player.x -= 25;
+    player.color = "rgb(255,202,40)";
+  };
+  right.onclick = () => {
+    player.x += 25;
+    player.color = "rgb(0,230,118)";
+  };
+
+  window.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowUp") {
+      player.y -= 25;
+      player.color = "rgb(179,157,219)";
+    }
+    if (e.key === "ArrowDown") {
+      player.y += 25;
+      player.color = "rgb(239,83,80)";
+    }
+    if (e.key === "ArrowRight") {
+      player.x += 25;
+      player.color = "rgb(0,230,118)";
+    }
+    if (e.key === "ArrowLeft") {
+      player.x -= 25;
+      player.color = "rgb(255,202,40)";
+    }
+  });
+
+  game();
+})();
